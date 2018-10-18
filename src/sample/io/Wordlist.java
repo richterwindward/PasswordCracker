e43251ca9b95326d8249f709b21ded62fbd8a8ea
richterwindward <rbrzeski19@windwardschool.org>
2018-10-18 10:51:01 -0700

    add password list

--- src/sample/io/Wordlist.java
+++ src/sample/io/Wordlist.java
@@ -1,4 +1,16 @@
 package sample.io;
 
+import java.io.File;
+
 public class Wordlist {
+    private File file;
+
+    public Wordlist(File file) {
+        this.file = file;
+    }
+
+    void ___________________get() {
+
+    }
+
 }
